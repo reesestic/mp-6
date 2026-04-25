4bc9b3d48b0086be193c277fa702ecc49bf375db
Reese Stichter <rsticht@bu.edu>
2026-04-24 20:58:14 -0400

    commit

--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    secret: process.env.AUTH_SECRET, // added b/c of vercel bugs
+    secret: process.env.AUTH_SECRET || "fallback_fake_secret",
     providers: [GitHub],
 });
