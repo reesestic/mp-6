404ee46fade2a550bb09ba344492453b5a13d678
Reese Stichter <rsticht@bu.edu>
2026-04-24 20:55:01 -0400

    added auth secret to NextAuth call b/c of vercel bugs, PLEASE work

--- auth.ts
+++ auth.ts
@@ -2,5 +2,6 @@ import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    secret: process.env.AUTH_SECRET, // added b/c of vercel bugs
     providers: [GitHub],
-})
+});
