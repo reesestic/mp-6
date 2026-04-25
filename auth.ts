fcc97d3c2281ed84d030589506c20059b8d049a2
Reese Stichter <rsticht@bu.edu>
2026-04-24 21:03:14 -0400

    fix auth import path

--- auth.ts
+++ auth.ts
@@ -2,6 +2,6 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    secret: process.env.AUTH_SECRET || "fallback_fake_secret",
+    secret: process.env.AUTH_SECRET || "fallback_secret_123",
     providers: [GitHub],
 });
