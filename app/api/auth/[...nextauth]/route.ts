62ccaef4d977120d9c7a0148cbdb5618f79e33c5
Reese Stichter <rsticht@bu.edu>
2026-04-24 21:00:49 -0400

    Changed import line

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 // app/api/auth/[...nextauth]/route.ts
 // file given by auth.js documentation, i copied the format
 
-import { handlers } from "@/auth"
+import { handlers } from "../../../../auth";
 
 export const { GET, POST } = handlers
